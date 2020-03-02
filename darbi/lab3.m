e0a1289b436c1e5a84f7b4d3d4a487e8c7ab2dac
user <lostpheonix6@gmail.com>
2020-03-02 09:21:35 +0200

    20200302_09_21

--- darbi/lab3.m
+++ darbi/lab3.m
@@ -1,31 +1,32 @@
 function y = lab3(t)
 if nargin == 0
-    t = 0:0.01:6.5;
+    t = 0:0.01:5;
 end
 %%Parejos laikus izteiksim ar datu filtraciju
-t_sin_f = (t>=1.5) & (t<3.5);
+%sin
+t_sin_f = (t>=4.5) & (t<=5);
 t_sin = t(t_sin_f);
-%t_sin= 1.5:0.01:3.5;
+%saw
 t_saw_f = (t>=0)&(t<0.5);
 t_saw = t(t_saw_f);
-%t_saw = 0:0.01:0.5;
-t_const_f = (t>=3.5)&(t<5);
+%const
+t_const_f = (t>=3.5)&(t<4.5);
 t_const = t(t_const_f);
-%t_const = 3.5:0.01:5;
-t_zero_f = (t>=0.5)&(t<1.5);
+%zero
+t_zero_f = (t>=1.5)&(t<3.5);
 t_zero=t(t_zero_f);
-%t_zero = 0.5:0.01:1.5;
-t_noise_f = (t>=5)&(t<=6.5);
+%noise
+t_noise_f = (t>=0.5)&(t<=1.5);
 t_noise=t(t_noise_f);
-%t_noise = 5:0.01:6.5;
+
 %% sinusoida
 A0=0; A= 1.5;
-T = (3.5-1.5)/2;f=1/T;
+T = (5-4.5)/2;f=1/T;
 delay = 1.5;
 y_sin=A0+A*sin(2*pi*f*(t_sin-delay));
 %plot(t_sin,y_sin)
 %axis([xmin xmax ymin ymax])
-axis([0 6.5 -2 2])
+%axis([0 5 -1.5 1.5])
 %% Lineari mainigais signals
 %k = (yA-yB)/(tA-tB);
 k = (1-0)/(0-0.5);
@@ -43,8 +44,8 @@ y_zero = zeros(size(t_zero));
 y_noise = 1.5*rand(size(t_noise))-0.75;
 %plot(t_noise,y_noise)
 %% Visu signalu apvienosana
-t = [t_saw,t_zero,t_sin,t_const,t_noise];
-y = [y_saw,y_zero,y_sin,y_const,y_noise];
+t = [t_saw,t_noise,t_zero,t_const,t_sin];
+y = [y_saw,y_noise,y_zero,y_const,y_sin];
 if nargout == 0
     plot(t,y)
     y=[];
